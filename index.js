a7f9b60418b4c09f3ce17eaeb3b9f7cd91edac1c
Brenda309 <brewihogora23@gmail.com>
2022-03-28 22:23:12 +0200

    Add the function that add the books.

---
+++ index.js
@@ -0,0 +1,19 @@
+const theBooks = [];
+
+const addButton = document.querySelector(".button");
+addButton.addEventListener('click', () => {
+    const bookContainer = document.createElement('div');
+    const titleBook = document.createElement('h5');
+    const authorBook = document.createElement('h5');
+    const removeButton = document.createElement('button');
+    const breakLine = document.createElement('hr');
+    const inputTitle = document.querySelector('.title');
+    const inputAuthor = document.querySelector('.author');
+    titleBook.innerHTML = inputTitle.value;
+    authorBook.innerHTML = inputAuthor.value;
+    removeButton.innerHTML = 'Remove';
+
+    bookContainer.append(titleBook, authorBook, removeButton, breakLine);
+    const bookList = document.querySelector('.book-list');
+    bookList.append(bookContainer);
+});
