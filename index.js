2403c2bff61be30541432eca6f466819cf851661
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-03-30 18:48:46 -0500

    Style the whole page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const addButton = document.querySelector('.button');
+const addButton = document.querySelector('#add-btn');
 const bookList = document.getElementsByClassName('book-list')[0];
 const inputTitle = document.querySelector('.title');
 const inputAuthor = document.querySelector('.author');
@@ -16,22 +16,27 @@ class BookObject {
   add() {
     function creaateAndAppend(title, author, id) {
       const bookContainer = document.createElement('div');
+      const infoContainer = document.createElement('div');
       const titleBook = document.createElement('h5');
       const authorBook = document.createElement('h5');
       const idBook = document.createElement('h5');
       const removeButton = document.createElement('button');
-      const breakLine = document.createElement('hr');
 
+      bookContainer.classList.add('book-container');
+      infoContainer.classList.add('book-info');
+      titleBook.classList.add('book-title');
+      authorBook.classList.add('book-author');
       removeButton.classList.add('remove-btn');
     
-      titleBook.innerHTML = title;
-      authorBook.innerHTML = author;
+      titleBook.innerHTML = `${title}&nbsp;`;
+      authorBook.innerHTML = `by ${author}&nbsp;`;
       idBook.innerHTML = id;
       removeButton.innerHTML = 'Remove';
     
-      // idBook.style.display = 'none';
+      idBook.style.display = 'none';
     
-      bookContainer.append(titleBook, authorBook, idBook, removeButton, breakLine);
+      infoContainer.append(titleBook, authorBook, idBook);
+      bookContainer.append(infoContainer, removeButton);
       bookList.append(bookContainer);
     
       BookObject.remove(removeButton);
@@ -41,11 +46,12 @@ class BookObject {
 
   static remove(element) {
     function removeBook() {
-      theBooks = theBooks.filter((book) => +book.id !== +this.parentNode.childNodes[2].innerHTML);
+      theBooks = theBooks.filter((book) => +book.id
+      !== +this.parentNode.children[0].children[2].innerHTML);
       this.parentNode.remove();
 
       theBooks.forEach((book, i) => {
-        bookList.children[i].children[2].innerHTML = i;
+        bookList.children[i].children[0].children[2].innerHTML = i;
         book.id = i;
       });
       localStorage.setItem('booksArray', JSON.stringify(theBooks));
@@ -67,8 +73,8 @@ function addBooks() {
 
     localStorage.setItem('booksArray', JSON.stringify(theBooks));
 
-    inputTitle.value = '';
-    inputAuthor.value = '';
+    // inputTitle.value = '';
+    // inputAuthor.value = '';
     alertMessage.style.display = 'none';
   } else {
     alertMessage.textContent = 'Empty field';
